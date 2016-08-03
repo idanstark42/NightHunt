29eb11056947d2d2e720a16400c8dd556c2f8c4e
Idan Stark <Idan Stark>
2016-08-04 01:06:04 +0300

    rails generated Teams model.

---
+++ db/migrate/20160803220438_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :password_digest
+      t.integer :points
+
+      t.timestamps null: false
+    end
+  end
+end
