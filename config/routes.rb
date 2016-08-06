f26f0ab0e03b84f019b6c34ee83170457a75866d
Idan Stark <Idan Stark>
2016-08-06 09:24:20 +0300

    Created home controller.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :teams
 
   post 'teams/login'
+  get 'home/status'
+  get 'home/index'
+
+  root 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
