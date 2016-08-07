51dca0397281201be732e673040558bf6e2a759b
Idan Stark <Idan Stark>
2016-08-07 22:18:13 +0300

    Changed admin permissions for expiriment.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803224946) do
+ActiveRecord::Schema.define(version: 20160807161534) do
 
   create_table "achievement_successes", force: :cascade do |t|
     t.integer  "achievement_id"
@@ -61,6 +61,25 @@ ActiveRecord::Schema.define(version: 20160803224946) do
 
   add_index "flags", ["area_id"], name: "index_flags_on_area_id"
   add_index "flags", ["team_id"], name: "index_flags_on_team_id"
+
+  create_table "puzzle_piece_reveals", force: :cascade do |t|
+    t.integer  "puzzle_piece_id"
+    t.integer  "team_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "puzzle_piece_reveals", ["puzzle_piece_id"], name: "index_puzzle_piece_reveals_on_puzzle_piece_id"
+  add_index "puzzle_piece_reveals", ["team_id"], name: "index_puzzle_piece_reveals_on_team_id"
+
+  create_table "puzzle_pieces", force: :cascade do |t|
+    t.integer  "x_coord"
+    t.integer  "y_coord"
+    t.string   "image_uri"
+    t.string   "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string   "name"
